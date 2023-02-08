85d7193390a042ebf9ff2511e8567bdecb9ab7be
BaltaKlixJones <balta98_@hotmail.com>
2023-02-08 16:18:02 -0300

    Commit Portfolio

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,20 +5,20 @@ import {  animateScroll as scroll } from "react-scroll";
 export default function NavBar () {
 
    
-    // const scrollToTop = () => {
-    //     scroll.scrollToTop();
-    // };
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
 
-    // const scrollToBottom = () => {
-    //     scroll.scrollToBottom()
-    // };
-    // const scrollToSkills = () => {
-    //     scroll.scrollTo(480, 0)
-    // }
+    const scrollToBottom = () => {
+        scroll.scrollToBottom()
+    };
+    const scrollToSkills = () => {
+        scroll.scrollTo(480, 0)
+    }
 
-    // const scrollToProyects = () => {
-    //     scroll.scrollTo(1180, 0)
-    // }
+    const scrollToProyects = () => {
+        scroll.scrollTo(1180, 0)
+    }
 
     return (
         
@@ -26,10 +26,10 @@ export default function NavBar () {
         <div className="nav-content">
           
           <ul  className="nav-items">
-            <li className="nav-item">Home</li>
-            <li  className="nav-item">Skills</li>
-            <li  className="nav-item">Proyects</li>
-            <li  className="nav-item">Contact</li>
+            <li onClick={scrollToTop} className="nav-item">Home</li>
+            <li onClick={scrollToSkills} className="nav-item">Skills</li>
+            <li onClick={scrollToProyects} className="nav-item">Proyects</li>
+            <li onClick={scrollToBottom} className="nav-item">Contact</li>
           </ul>
         </div>
       </nav>
