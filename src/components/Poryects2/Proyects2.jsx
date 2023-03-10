2dbacbd39026aa81ce14d7fca8f4a50cc93ede8d
BaltaKlixJones <balta98_@hotmail.com>
2023-03-10 12:19:39 -0300

    Cards

--- src/components/Poryects2/Proyects2.jsx
+++ src/components/Poryects2/Proyects2.jsx
@@ -1,55 +1,82 @@
 import "./Proyects2.css";
 import React from "react";
 
-
 export default function Proyects2() {
-    return (       
- <div className="card-conteiner"> 
-  <div className="card" >
-  <div class="tools">
-    <div class="circle">
-      <span class="red box"></span>
-    </div>
-    <div class="circle">
-      <span class="yellow box"></span>
-    </div>
-    <div class="circle">
-      <span class="green box"></span>
-    </div>
-  </div>
-      <h2>Pokemon</h2>
-      <img src="https://www.pngplay.com/wp-content/uploads/10/Pokemon-Free-Picture-PNG.png" alt="" />
-      <h1>Proyecto Individual</h1>
-      <h3>Uso de la API de Pokemon</h3>
-      
-      <button class="cssbuttons-io">
-  <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"></path></svg> Code</span>
-</button>
-    </div>
+  return (
+    <div className="card-conteiner">
+      <div className="card">
+        <div class="tools">
+          <div class="circle">
+            <span class="red box"></span>
+          </div>
+          <div class="circle">
+            <span class="yellow box"></span>
+          </div>
+          <div class="circle">
+            <span class="green box"></span>
+          </div>
+        </div>
+        <h2>Pokemon</h2>
+        <img
+          src="https://www.pngplay.com/wp-content/uploads/10/Pokemon-Free-Picture-PNG.png"
+          alt=""
+        />
+        <h1>Proyecto Individual</h1>
+        <h3>Uso de la API de Pokemon</h3>
+        <a href="about:blank" target="_blank">
+          <button class="cssbuttons-io">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path
+                  fill="currentColor"
+                  d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+                ></path>
+              </svg>
+              Deploy
+            </span>
+          </button>
+        </a>
+      </div>
 
-    <div className="card" >
-    <div class="tools">
-    <div class="circle">
-      <span class="red box"></span>
-    </div>
-    <div class="circle">
-      <span class="yellow box"></span>
-    </div>
-    <div class="circle">
-      <span class="green box"></span>
-    </div>
-  </div>
-      <h2>Flex Agenda</h2>
-      <button className="card-content">Web</button>
-      <img style={{height:"220px", width:"220px", marginLeft:"10px"}}src="https://static.vecteezy.com/system/resources/previews/010/160/301/non_2x/calendar-icon-sign-symbol-design-free-png.png" alt="" />
-      <h1>Flex Agenda</h1>
-      <h3>React, Node.js, Express, PostgreSQL</h3>
-      <button class="cssbuttons-io">
-  <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"></path></svg> Code</span>
-</button>
-    </div>
+      <div className="card">
+        <div class="tools">
+          <div class="circle">
+            <span class="red box"></span>
+          </div>
+          <div class="circle">
+            <span class="yellow box"></span>
+          </div>
+          <div class="circle">
+            <span class="green box"></span>
+          </div>
+        </div>
+        <h2>Flex Agenda</h2>
+        <button className="card-content">Web</button>
+        <img
+          style={{ height: "220px", width: "220px", marginLeft: "10px" }}
+          src="https://static.vecteezy.com/system/resources/previews/010/160/301/non_2x/calendar-icon-sign-symbol-design-free-png.png"
+          alt=""
+        />
+        <h1>Flex Agenda</h1>
+        <h3>React, Node.js, Express, PostgreSQL</h3>
+        <a href="https://flex-agenda.vercel.app/" target="_blank">
+          <button class="cssbuttons-io">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path
+                  fill="currentColor"
+                  d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+                ></path>
+              </svg>{" "}
+              Deploy
+            </span>
+          </button>
+        </a>
+      </div>
 
-        <div className="card" >
+      {/* <div className="card" >
         <div class="tools">
     <div class="circle">
       <span class="red box"></span>
@@ -70,30 +97,46 @@ export default function Proyects2() {
   <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"></path></svg> Code</span>
 </button>
 
+    </div> */}
+
+      <div className="card">
+        <h2>Vehiculos</h2>
+        <div class="tools">
+          <div class="circle">
+            <span class="red box"></span>
+          </div>
+          <div class="circle">
+            <span class="yellow box"></span>
+          </div>
+          <div class="circle">
+            <span class="green box"></span>
+          </div>
+        </div>
+        <img
+          style={{ height: "225px" }}
+          src="https://i.pinimg.com/originals/0f/6e/a6/0f6ea6a66666af19f0739fa60055b9f4.png"
+          alt=""
+        />
+        <h1>Vehiculos</h1>
+        <h3>Python, Django</h3>
+        <a
+          href="https://github.com/BaltaKlixJones/Proyecto-Python-Autos"
+          target="_blank"
+        >
+          <button class="cssbuttons-io">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path
+                  fill="currentColor"
+                  d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+                ></path>
+              </svg>{" "}
+              Code
+            </span>
+          </button>
+        </a>
+      </div>
     </div>
-        <div className="card" >
-        
-      <h2>Vehiculos</h2>
-      <div class="tools">
-    <div class="circle">
-      <span class="red box"></span>
-    </div>
-    <div class="circle">
-      <span class="yellow box"></span>
-    </div>
-    <div class="circle">
-      <span class="green box"></span>
-    </div>
-  </div>
-      <img style={{height: "225px",}} src="https://i.pinimg.com/originals/0f/6e/a6/0f6ea6a66666af19f0739fa60055b9f4.png" alt="" />
-     <h1>Vehiculos</h1>
-     <h3>Python, Django</h3>
-     <button class="cssbuttons-io">
-  <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"></path></svg> Code</span>
-</button>
-    </div>
-    
-  </div>
-    )
+  );
 }
-            
